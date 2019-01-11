c1bf681ad1b0d64b2ae61f0b24c00a80a6a97123
Alumno08 J901 <a08j901@campustls.local>
2019-01-10 20:42:23 -0500

    agrego js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded', function(){
-  alert("Hola");
-}),
+document.addEventListener('DOMContentLoaded', function() {
+  const botonComprar = document.querySelectorAll(".boton-comprar")[0];
+  botonComprar.addEventListener("click", function() {
+    alert("gracias por su compra");
+  })
+});
